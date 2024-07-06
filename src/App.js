a9f042c4f58b5fa8f07454399c68f2ee380444cd
emefaAngela <gelamengor@gmail.com>
2024-07-06 10:14:02 +0000

    fetching local data from json file

--- src/App.js
+++ src/App.js
@@ -4,13 +4,39 @@ import { Button } from './components/Button';
 import { Job } from './components/Job';
 import { Tag } from './components/Tag';
 import { Logo } from './components/Logo';
+import { useEffect } from 'react';
 
 //App component
 function App() {
+
+//Getting json data
+const getData=()=>{
+  fetch('data.json',
+    {
+      headers:{
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    }
+  )
+  .then(function(response){
+    console.log(response)
+    return response.json()
+  })
+}
+useEffect(()=>{
+  getData()
+},[])
+
   return (
     <div className="w-full h-100vh bg-[color:hsl(180,31%,95%)]">
       <div className='w-full h-[8em]  bg-[color:hsl(180,29%,50%)]'></div>
-      <Logo >New</Logo>
+      <div className='absolute flex  w-10/12 top-0 flex-col  my-24 mx-8 sm:mx-32  justify-center items-center'>
+          <div className=' rounded-sm h-[4em] w-full flex flex-row justify-between items-center px-4 bg-[color:hsl(180,31%,95%)]'>
+            <div></div>
+            <p className='underline text-[color:hsl(180,29%,50%)] '>Clear</p>
+          </div>
+      </div>
     </div>
   );
 }
