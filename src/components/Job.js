10b21a41087db2a8f714de726993e7a0a78310eb
emefaAngela <gelamengor@gmail.com>
2024-07-06 12:27:55 +0000

    debugging path to fetch logo of job item

--- src/components/Job.js
+++ src/components/Job.js
@@ -6,7 +6,7 @@ import { Logo } from "./Logo";
 const Job = ({jobItem}) => {
   return ( 
     <div className="w-full h-8em rounded-sm p-4 flex flex-col sm:flex-row sm:space-x-4">
-      <Logo/>
+      <Logo src={process.env.PUBLIC_URL + jobItem.logo}/>
       <div className="flex flex-col sm:flex-row sm:justify-between"> {jobItem.company}</div>
 
     </div>
--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,8 +1,10 @@
 import styled from 'styled-components'
 
 //Logo component
-export const Logo = styled.div`
+export const Logo = styled.img`
 border-radius: 8em;
 width: 6em;
 height: 6em;
+object-fit: cover;
+
 `
