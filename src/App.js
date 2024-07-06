61cc77b341d328ad97e345974182df454fcff362
emefaAngela <gelamengor@gmail.com>
2024-07-06 09:37:03 +0000

    styling tag component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@
 import './App.css';
 import { Button } from './components/Button';
 import { Job } from './components/Job';
+import { Tag } from './components/Tag';
 
 //App component
 function App() {
   return (
     <div className="w-full h-100vh bg-[color:hsl(180,31%,95%)]">
       <div className='w-full h-[8em]  bg-[color:hsl(180,29%,50%)]'></div>
-      <Button >New</Button>
+      <Tag >New</Tag>
     </div>
   );
 }
--- src/components/Tag.js
+++ src/components/Tag.js
@@ -2,13 +2,14 @@ import styled, {css} from 'styled-components'
 
 //Button component
 export const Tag = styled.button`
-  background: #FFFFFF;
+  background: hsl(180,31%,95%);
   border-radius: 3px;
-  border: 2px solid #BF4F74;
-  color: #BF4F74;
+  color: hsl(180,29%,50%);
   margin: 0 1em;
   padding: 0.25em 1em;
  
-   
+  &:hover {
+  color:white;
+  }  
 
 `
