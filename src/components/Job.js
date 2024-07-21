19161d2270d03972665ff3ab884ed9f35852b41f
emefaAngela <gelamengor@gmail.com>
2024-07-21 04:36:01 +0000

    minor fix

--- src/components/Job.js
+++ src/components/Job.js
@@ -3,10 +3,17 @@ import { Button } from "./Button"
 import { Tag } from "./Tag"
 import { Logo } from "./Logo"
 import { tw } from "../utils/tailwind"
+import React from "react"
 
 
 //Job component
 const Job = ({jobItem,handleFilterClick}) => {
+
+  //
+  const items = [jobItem.postedAt, jobItem.contract, jobItem.location];
+  const separator = ".";
+
+  //Render
   return ( 
     <div className={tw(
       'w-full h-8em',
@@ -55,15 +62,16 @@ const Job = ({jobItem,handleFilterClick}) => {
         )}>
           {jobItem.position}
         </div>
-      <div className={tw("flex space-x-2 text-[hsl(180,8%,52%)]  font-medium")}>
         <div className={tw(
-          'text-[color:hsl(180, 52%, 96%)]'
-        )}>{jobItem.postedAt}</div>
-        <div className="">.</div>
-        <div>{jobItem.contract}</div>
-        <div>.</div>
-        <div>{jobItem.location}</div>
-      </div>
+          "flex space-x-2 text-[hsl(180,8%,52%)]  font-medium"
+          )}>
+      {items.map((item, index) => (
+        <React.Fragment key={index}>
+          <div>{item}</div>
+          {index < items.length - 1 && <div>{separator}</div>}
+        </React.Fragment>
+      ))}
+    </div>
       </div>
       </div>
       <div className={tw(
